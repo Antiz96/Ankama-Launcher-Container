ef3c0632a1d891a670ef2cce85cf439b4c1c02e6
Robin Candau <robincandau@protonmail.com>
2023-08-09 23:34:18 +0200

    Remove absolute path for the distrobox binary

--- src/script/ankama-launcher-container.sh
+++ src/script/ankama-launcher-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xhost +si:localuser:"${USER}" &>/dev/null ; /usr/bin/distrobox-enter -r -n ankama-launcher -- /usr/local/bin/ankama-launcher
+xhost +si:localuser:"${USER}" &>/dev/null ; distrobox-enter -r -n ankama-launcher -- /usr/local/bin/ankama-launcher
